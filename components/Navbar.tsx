772c22f251e3e48d4ec9451276e86effdfed9de0
Leila <leilaabdulahad@hotmail.com>
2024-09-10 13:30:52 +0200

    adds navlinks file and create routing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,8 @@ import {
   SignedIn,
   SignedOut,
   UserButton
-} from '@clerk/nextjs';
+} from '@clerk/nextjs'
+import { navLinks } from '@/constants/navLinks'
 
 const Navbar = (): JSX.Element => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
@@ -26,72 +27,45 @@ const Navbar = (): JSX.Element => {
           </Link>
         </div>
 
-        <nav className="hidden md:flex space-x-8 font-sans font-bold">
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <Link href="/">
-            <div>Hem</div>
-          </Link>
-          <Link href="/profile">
-            <div>Profil</div>
-          </Link>
-          <Link href="/categories">
-            <div>Kategorier</div>
-          </Link>
-          <Link href="/about">
-            <div>Om oss</div>
-          </Link>
-          <Link href="/contact">
-            <div>Kontakt</div>
-          </Link>
-          <Link href="/search">
-            <div className="flex items-center space-x-1">
-              <div>Sök</div>
-              <FaSearch className="text-gray-400" size={14} />
-            </div>
-          </Link>
-        </nav>
+    <nav className="hidden md:flex space-x-8 font-sans font-bold">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      {navLinks.map((link) => (
+        <Link href={link.path} key={link.name}>
+          <div className="flex items-center space-x-1">
+            <div>{link.name}</div>
+            {link.isSearch && <FaSearch className="text-gray-400" size={14} />}
+          </div>
+        </Link>
+      ))}
+    </nav>
 
         <div className="md:hidden flex items-center">
           <FaBars className="text-2xl cursor-pointer" onClick={toggleMobileMenu} />
         </div>
       </div>
       
-        {/* Mobile navigation */}
         {isMobileMenuOpen && (
           <nav className="md:hidden text-center p-4 space-y-2 text-white rounded-lg shadow-lg font-sans font-bold">
-            <Link href="/">
-              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">Hem</div>
-            </Link>
-            <SignedOut>
+          <SignedOut>
             <SignInButton>Logga in</SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-            <Link href="/categories">
-              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">Profil</div>
-            </Link>
-            <Link href="/profile">
-              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">Kategorier</div>
-            </Link>
-            <Link href="/about">
-              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">Om oss</div>
-            </Link>
-            <Link href="/contact">
-              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">Kontakt</div>
-            </Link>
-            <Link href="/search">
-              <div onClick={toggleMobileMenu} className="flex items-center justify-center space-x-1 p-2 hover:bg-gray-700 rounded">
-                <div>Sök</div>
-                <FaSearch className="text-gray-400" size={14} />
+          {navLinks.map((link) => (
+            <Link href={link.path} key={link.name}>
+              <div onClick={toggleMobileMenu} className="p-2 hover:bg-gray-700 rounded">
+                {link.name}
+                {link.isSearch && <FaSearch className="text-gray-400" size={14} />}
               </div>
             </Link>
-          </nav>
+          ))}
+        </nav>
         )}
         </header>
         )
---
+++ constants/navLinks.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+    { name: 'Hem', path: '/' },
+    { name: 'Profil', path: '/profile' },
+    { name: 'Skapa tråd', path: '/create' },
+    { name: 'Om oss', path: '/about' },
+    { name: 'Kontakt', path: '/contact' },
+
+]
